5c6492297e06d3574d170458ae58cfe9b376bcc2
Anil Kumar Gurindapalli <gurindapalli.anil@gmail.com>
2021-04-12 20:02:30 +0530

    Input Changes

--- C#InputTemplate.cs
+++ C#InputTemplate.cs
@@ -1,9 +1,75 @@
 using System;
+using Utils.Lib;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    private const int M = (int) 1e7;
+    InputScanner sc = new InputScanner();
+    public void Solve()
     {
+
+    }
+    static void Main() {
+        new Program().Solve();
+    }
+}
+
+namespace Utils.Lib
+{
+    using System;
+    using System.Linq;
+    class InputScanner
+    {
+       private string[] _line;
+       private int _index;
+       private const char Separator = ' ';
        
+       public InputScanner()
+       {
+           _line = new string[0];
+           _index = 0;
+       }
+       
+       public string Next()
+       {
+           if (_index >= _line.Length)
+            {
+                string s;
+                do
+                {
+                    s = Console.ReadLine();
+                } while (s.Length == 0);
+ 
+                _line = s.Split(Separator);
+                _index = 0;
+            }
+ 
+            return _line[_index++];
+       }
+       public string ReadLine()
+        {
+            _index = _line.Length;
+            return Console.ReadLine();
+        }
+ 
+        public int ReadInt() => int.Parse(Next());
+        public long ReadLong() => long.Parse(Next());
+        public double ReadDouble() => double.Parse(Next());
+        public decimal ReadDecimal() => decimal.Parse(Next());
+        public char ReadChar() => Next()[0];
+        public char[] ReadCharArray() => Next().ToCharArray();
+ 
+        public string[] Array()
+        {
+            string s = Console.ReadLine();
+            _line = s.Length == 0 ? new string[0] : s.Split(Separator);
+            _index = _line.Length;
+            return _line;
+        }
+ 
+        public int[] IntArray() => Array().Select(int.Parse).ToArray();
+        public long[] LongArray() => Array().Select(long.Parse).ToArray();
+        public double[] DoubleArray() => Array().Select(double.Parse).ToArray();
+        public decimal[] DecimalArray() => Array().Select(decimal.Parse).ToArray();
     }
 }
